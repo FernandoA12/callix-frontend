d8aa14d62302f73b4ace8bc87bf0ea78550bc165
Fernando Souza <fernando.souza@doxacode.com.br>
2022-08-30 14:42:47 -0300

    test: get upcomings launch

---
+++ backend/tests/GetUpcomingsLaunch.test.ts
@@ -0,0 +1,26 @@
+import { GetUpcomingsLaunch } from "./../application/GetUpcomingsLaunch";
+import { Launch } from "../domain/entities/Launch";
+import { LaunchesRepository } from "../domain/repositories/LaunchesRepository";
+
+it("Should get the upcomings rocket`s launch", async () => {
+  const launchesRepository: Pick<LaunchesRepository, "upcomingLaunches"> = {
+    upcomingLaunches: jest.fn(async () => [
+      new Launch({
+        name: "any",
+        date: "any",
+        details: "any",
+        rocket: {
+          name: "any",
+          description: "any",
+          thumbnails: ["any"],
+          type: "any",
+        },
+        thumbnail: "any",
+      }),
+    ]),
+  };
+  const getUpcomingsLaunch = new GetUpcomingsLaunch(launchesRepository);
+  const upcomingsLaunch = await getUpcomingsLaunch.execute();
+  expect(upcomingsLaunch[0].name).toBe("any");
+  expect(launchesRepository.upcomingLaunches).toBeCalled();
+});
