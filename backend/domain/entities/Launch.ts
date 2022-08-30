3e663496e112856e8f979b8374f0ca94d16c6298
Fernando Souza <fernando.souza@doxacode.com.br>
2022-08-30 13:21:40 -0300

    feat: add entity launch

---
+++ backend/domain/entities/Launch.ts
@@ -0,0 +1,24 @@
+interface Rocket {
+  name: string;
+  type: string;
+  description: string;
+  thumbnails: string[];
+}
+
+export class Launch {
+  public name!: string;
+  public details!: string;
+  public thumbnail!: string;
+  public date!: string;
+  public rocket!: Rocket;
+
+  constructor(props: Partial<Launch>) {
+    Object.assign(this, props);
+  }
+
+  toData() {
+    const { toData, ...lauch } = this;
+
+    return lauch;
+  }
+}
