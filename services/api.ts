4228e4ad97ed1a94797d31c81bb83eb4c0f64792
Fernando Souza <fernando.souza@doxacode.com.br>
2022-08-31 19:03:48 -0300

    ci: change api base url

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? "" : "http://localhost:3333",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://callix-backend.herokuapp.com"
+      : "http://localhost:3333",
 });
